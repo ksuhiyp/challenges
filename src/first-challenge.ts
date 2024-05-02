3295b05f91a7fa8bb2f35a79f0ea4ba417fec04e
suhayb <ksuhiyp@gmail.com>
2024-05-02 17:00:05 +0300

    Refactor copyFile function to improve performance and handle errors

--- src/first-challenge.ts
+++ src/first-challenge.ts
@@ -1,35 +1,6 @@
 import { createReadStream, createWriteStream } from "fs";
 
 export const copyFile = () => {
-    const readStream = createReadStream('./data/big.csv');
-    const writeStream = createWriteStream('./data/copy.csv');
-
-    readStream.on('data', (chunk) => {
-        writeStream.write(chunk);
-        if (writeStream.write(chunk) === false) {
-            readStream.pause();
-        }
-    }
-
-    );
-
-    writeStream.on('drain', () => {
-        console.log('drain');
-        readStream.resume();
-    }
-
-    );
-
-
-    readStream.on('close', () => {
-        console.log('done');
-    });
-
-    readStream.on('error', (error) => {
-        console.log('error', error);
-    });
-
-
 }
 
 copyFile();
